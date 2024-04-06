ccd1bd422b7362c259243c4972ebab4f9714c834
Sergii Podriezienko <podreenko1986@gmail.com>
2024-04-06 13:44:10 +0300

    add: new date
    add: new date

--- my-first-project/vue.config.js
+++ my-first-project/vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "/new_vue_project/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/my-first-project/" : "/",
 });
